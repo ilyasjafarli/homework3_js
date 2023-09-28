f82df4ded69a8d94b33b8c6aac5d8f718185f45b
ilyasjafarli <ilyas.cefer@gmail.com>
2023-09-29 00:46:18 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,23 @@
+
+
+let from = 30;
+let to = 50;
+
+for (let s = from; s<= to; s++) {
+    let value = s;
+    let isPrime = true;
+for (let f = 2; f < value; f++) {
+    if (value % f == 0) {
+        isPrime = false;
+        break;
+    }
+} 
+
+if (isPrime == true) {
+    let sum = 0;
+for (let h = 0; h< to; h++ )
+{
+    console.log($.sum);
+    }
+}
+}
